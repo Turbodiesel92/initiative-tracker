ba61c42f514da4496ade2258060491912d5f88cd
MatthewWilliams <turbodieseldodge@gmail.com>
2023-04-03 17:06:59 -0500

    start to fill db

--- server/models.py
+++ server/models.py
@@ -1,5 +1,54 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-from config import db
+db = SQLAlchemy()
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.Varchar)
+    campaign = db.Column(db.String)
+
+
+class PlayerCharacter(db.Model, SerializerMixin):
+    __tablename__ = 'player_characters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String)
+    initiative = db.Column(db.Integer)
+    character_classes = db.Column(db.String)
+
+class NonPlayerCharacter(db.Model, SerializerMixin):
+    __tablename__ = 'non_player_characters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    npc_name = db.Column(db.String)
+    initiative = db.Column(db.Integer)
+
+class CharacterClass(db.Model, SerializerMixin):
+    __tablename__ = 'character_classes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    character_classes = db.Column(db.String)
+    campaign_id = db.Column(db.Integer)
+    character_name = db.Column(db.String)
+
+class Campaign(db.Model, SerializerMixin):
+    __tablename__ = 'campaigns'
+
+    id = db.Column(db.Integer, primary_key=True)
+    campaign = db.Column(db.String)
+
+class TurnTracker(db.Model, SerializerMixin):
+    __tablename__ = 'turn_trackers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.foreignKey('users.id'))
+    player_characters_id = db.Column(db.Integer, db.ForeignKey('player_characters.id'))
+    non_player_characters_id = db.Column(db.Integer, db.ForeignKey('non_player_characters.id'))
+    character_initiative = db.Column(db.Integer)
+    non_player_character_initiative = db.Column(db.Integer)
