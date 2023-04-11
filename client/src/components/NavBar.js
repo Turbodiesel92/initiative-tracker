1b9c9366b8f349dfeb3c7c39a93445e86d2dd9da
MatthewWilliams <turbodieseldodge@gmail.com>
2023-04-11 12:46:02 -0500

    incorperated usecontext

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Button } from "../styles";
+import { UserContext } from './App'
 
-function NavBar({ user, setUser }) {
+
+function NavBar() {
   function handleLogoutClick() {
     fetch("/logout", { method: "DELETE" }).then((r) => {
       if (r.ok) {
@@ -11,6 +13,8 @@ function NavBar({ user, setUser }) {
       }
     });
   }
+
+  const [user, setUser] = useContext(UserContext)
 
   return (
     <Wrapper>
