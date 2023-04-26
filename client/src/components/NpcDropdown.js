7e132356dd6fa098614627339e0d1739577d56e6
MatthewWilliams <turbodieseldodge@gmail.com>
2023-04-26 11:55:36 -0500

    setup npc dropdown

--- client/src/components/NpcDropdown.js
+++ client/src/components/NpcDropdown.js
@@ -6,12 +6,12 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-function Campaign() {
+function NpcDropdown() {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          Campaigns
+          NPC Name
           <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
         </Menu.Button>
       </div>
@@ -60,4 +60,4 @@ function Campaign() {
   )
 }
 
-export default Campaign
+export default NpcDropdown
