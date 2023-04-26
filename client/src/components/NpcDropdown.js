c431ef820437a8ffc4c0c9f3805b1a8501ecb5da
MatthewWilliams <turbodieseldodge@gmail.com>
2023-04-26 11:40:18 -0500

    working on npc dropdown menu

---
+++ client/src/components/NpcDropdown.js
@@ -0,0 +1,71 @@
+import { Fragment, useState, useEffect } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/solid';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+function NpcDropdown() {
+  const [npcs, setNpcs] = useState([]);
+
+  useEffect(() => {
+    fetch('/npc')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Data received:', data);
+        setNpcs(data);
+
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    console.log('State updated:', npcs);
+  }, [npcs]);
+
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          NPC name
+          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {console.log('npcs:', npcs)}
+          <div className="py-1">
+
+            {npcs.map((npc) => (
+              <Menu.Item key={npc.id}>
+                {({ active }) => (
+                  <a
+                    href={`/npc/${npc.id}`}
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    {npc.name}
+                  </a>
+                )}
+              </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
+
+export default NpcDropdown;
