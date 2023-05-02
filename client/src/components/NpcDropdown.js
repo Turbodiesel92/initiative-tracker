e482cc112ca78fc42a49383109ace0838e88ff9c
MatthewWilliams <turbodieseldodge@gmail.com>
2023-05-02 16:35:14 -0500

    got the modal working, edit still not

--- client/src/components/NpcDropdown.js
+++ client/src/components/NpcDropdown.js
@@ -1,22 +1,25 @@
 import { Fragment, useEffect, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
-import { ChevronDownIcon, TrashIcon } from "@heroicons/react/solid";
+import { ChevronDownIcon, TrashIcon, PencilAltIcon } from "@heroicons/react/solid";
+import EditNpcModal from "./EditNpcModal";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-function NpcDropdown( { onNpcAdded }) {
+
+
+function NpcDropdown({ onNpcAdded }) {
   const [npcs, setNpcs] = useState([]);
-  const [npcList, setNpcList] = useState([])
+  const [npcList, setNpcList] = useState([]);
   const [error, setError] = useState(null);
-  const [editingNpc, setEditingNpc] = useState(null);
+  const [editingNpc, setEditingNpc] = useState(false);
 
   useEffect(() => {
     fetch("/npc")
       .then((response) => response.json())
       .then((data) => {
-        setNpcs(data.map(npc => [npc.id, npc.npc_name ]));
+        setNpcs(data.map((npc) => [npc.id, npc.npc_name]));
       })
       .catch((error) => {
         setError(error);
@@ -28,48 +31,55 @@ function NpcDropdown( { onNpcAdded }) {
     if (!npc) {
       return;
     }
-    const newName = prompt(`Enter a new name for ${npc[1]}`);
+    setEditingNpc(npc);
+  };
+
+  const handleEditSubmit = (event) => {
+    event.preventDefault();
+    const newName = event.target.elements.name.value;
     if (!newName) {
-      return
+      return;
     }
-    fetch(`/npc/${npcId}`, {
-      method:'PATCH',
+
+    fetch(`/npc/${editingNpc[0]}`, {
+      method: "PATCH",
       headers: {
         "content-Type": "application/json",
       },
       body: JSON.stringify({ npc_name: newName }),
     })
-    .then(response => {
-      if (response.ok) {
-        setNpcs((prevNpcs) =>
-          prevNpcs.map((npc) =>
-           npc[0] === npcId ? [npcId, newName] : npc));
-      } else {
-        throw new Error('Failed to update NPC')
-      }
-    })
-    .catch(error => {
-      setError(error)
-    })
-  }
+      .then((response) => {
+        if (response.ok) {
+          setNpcs((prevNpcs) =>
+            prevNpcs.map((npc) =>
+              npc[0] === editingNpc[0] ? [editingNpc[0], newName] : npc
+            )
+          );
+          setEditingNpc(null);
+        } else {
+          throw new Error("Failed to update NPC");
+        }
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  };
 
   const handleDeleteClick = (npcId) => {
     fetch(`/npc/${npcId}`, {
-      method:'DELETE',
+      method: "DELETE",
     })
-    .then(response => {
-      if (response.ok) {
-        setNpcs((prevNpcs) =>
-          prevNpcs.filter((npc) =>
-           npc[0] !== npcId));
-      } else {
-        throw new Error('Failed to delete NPC')
-      }
-    })
-    .catch(error => {
-      setError(error)
-    })
-  }
+      .then((response) => {
+        if (response.ok) {
+          setNpcs((prevNpcs) => prevNpcs.filter((npc) => npc[0] !== npcId));
+        } else {
+          throw new Error("Failed to delete NPC");
+        }
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  };
 
   return (
     <>
@@ -97,7 +107,7 @@ function NpcDropdown( { onNpcAdded }) {
             {npcs.length > 0 && (
               <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div className="py-1">
-                  {npcs.map(npc => {
+                  {npcs.map((npc) => {
                     if (!npc[1]) {
                       return null;
                     }
@@ -105,29 +115,31 @@ function NpcDropdown( { onNpcAdded }) {
                       <Menu.Item key={`menu-${npc[0]}`}>
                         {({ active }) => (
                           <div className="flex items-Center justify-between">
-                          <button
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-grey-900"
-                                : "text-grey-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                            onClick={() => handleEditClick(npc[0])}
-                          >
-                            {npc[1]}
-                          </button>
-                          <button
+                            <button
+                              href="#"
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-grey-900"
+                                  : "text-grey-700",
+                                "block px-4 py-2 text-sm"
+                              )}
+                              onClick={() => handleEditClick(npc[0])}
+                            >
+                              {npc[1]}
+                              <PencilAltIcon className='h-5 w-5' />
+                            </button>
+                            <button
                               href="#"
                               className="text-gray-400 hover:text-gray-500 w-5 h-5"
                               onClick={() => handleDeleteClick(npc[0])}
-                              >
-                                <TrashIcon />
-                              </button>
-                              </div>
+                            >
+
+                              <TrashIcon className='h-5 w-5' />
+                            </button>
+                          </div>
                         )}
                       </Menu.Item>
-                    )
+                    );
                   })}
                 </div>
               </Menu.Items>
@@ -135,6 +147,16 @@ function NpcDropdown( { onNpcAdded }) {
           </Transition>
         </Menu>
       )}
+
+      {editingNpc && (
+        <EditNpcModal
+          npc={editingNpc}
+          onSubmit={handleEditSubmit}
+          onClose={() => setEditingNpc(null)}
+          />
+      )}
+
+      {error && <p className="text-red-500">{error.message}</p>}
     </>
   );
 }
